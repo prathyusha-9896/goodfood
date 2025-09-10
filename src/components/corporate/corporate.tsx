0eee0edb1d35ca9eb45c0344c2fe8aa66bbad6b2
prathyusha-9896 <jfsjd.mum.prathyusha@gmail.com>
2025-09-10 18:57:03 +0530

    logos first section alignment

--- src/components/corporate/corporate.tsx
+++ src/components/corporate/corporate.tsx
@@ -7,7 +7,7 @@ export default function Corporate() {
   return (
     <section className="grid grid-cols-1 md:grid-cols-2 min-h-screen">
       {/* Left Side */}
-      <div className="flex flex-col justify-center md:justify-start md:items-start pt-16 md:pt-32 items-center px-8 md:px-36 bg-[#FCFAF4] text-white">
+      <div className="flex flex-col justify-center md:justify-start md:items-start pt-16 md:pt-32 items-center px-8 md:px-32 bg-[#FCFAF4] text-white">
         <h1 style={{ fontFamily: "Albra, serif" }} className="text-[42px] md:text-5xl md:text-left text-center font-bold text-[#333333] leading-tight mb-7">
           {headline}
         </h1>
@@ -16,11 +16,17 @@ export default function Corporate() {
         </button>
 
         <p className="mt-14 text-[12px] text-[#808080] font-semibold leading-[137%]">{tagline}</p>
-        <div className="flex flex-wrap md:flex-nowrap justify-center md:justify-start items-center md:items-start gap-6 my-6">
+        <div className="flex flex-wrap md:flex-nowrap justify-center items-center md:justify-start md:items-start gap-4 my-6">
           {logos.map((logo, idx) => (
-            <img key={idx} src={logo.src} alt={logo.alt}  />
+            <img
+              key={idx}
+              src={logo.src}
+              alt={logo.alt}
+              className="w-[100px] h-[35px] object-contain"
+            />
           ))}
         </div>
+
       </div>
 
       {/* Right Side */}
